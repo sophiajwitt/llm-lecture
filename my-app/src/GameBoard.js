2295c331af527b029081beee80fa7930a887777a
sophiajwitt <sophiawitt33@gmail.com>
2023-04-06 16:16:20 -0400

    player win feature implemented

--- my-app/src/GameBoard.js
+++ my-app/src/GameBoard.js
@@ -5,8 +5,7 @@ function GameBoard() {
   const [board, setBoard] = useState(Array(9).fill(null)); // Initialize board with 9 null values
   const [isPlayer1Turn, setIsPlayer1Turn] = useState(true); // Initialize with player 1's turn
 
-  // Function to handle a cell click
-    const handleCellClick = (index) => {
+  const handleCellClick = (index) => {
     if (board[index] !== null) {
       return; // Do nothing if cell is already filled
     }
@@ -14,8 +13,38 @@ function GameBoard() {
     const newBoard = [...board]; // Create a copy of the board array
     newBoard[index] = isPlayer1Turn ? 'X' : 'O'; // Update the selected cell with X or O
     setBoard(newBoard); // Update the state with the new board array
+    
+    // Check for a win condition
+    const winningCombinations = [    // Horizontal    [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Vertical
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonal
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (newBoard[a] && newBoard[a] === newBoard[b] && newBoard[a] === newBoard[c]) {
+        // A player has won, display a message and highlight the winning combination
+        const message = `Player ${isPlayer1Turn ? '1' : '2'} has won!`;
+        alert(message);
+        
+        // Highlight the winning cells
+        const winningCells = document.querySelectorAll(`.cell:nth-child(${a + 1}), .cell:nth-child(${b + 1}), .cell:nth-child(${c + 1})`);
+        for (let cell of winningCells) {
+          cell.classList.add('winning');
+        }
+      }
+    }
+  
     setIsPlayer1Turn(!isPlayer1Turn); // Toggle the turn between players
   };
+  
 
   // Function to render a single cell
   const renderCell = (index) => {
